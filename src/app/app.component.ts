7fdd2b17fe0c7a20dbdf6d09a6751eed2e0526e1
adrianrakiec <rakiecadrian17@gmail.com>
2024-07-09 16:36:45 +0200

    build: create main-content component and basic styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-import { TitleComponent } from './title/title.component';
-import { ThirdBlockComponent } from './third-block/third-block.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, TitleComponent,ThirdBlockComponent],
+  imports: [RouterOutlet, HeaderComponent, MainContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/main-content/main-content.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { TitleComponent } from '../title/title.component';
+import { FirstBlockComponent } from '../first-block/first-block.component';
+import { SecondBlockComponent } from '../second-block/second-block.component';
+import { ThirdBlockComponent } from '../third-block/third-block.component';
+
+@Component({
+  selector: 'app-main-content',
+  standalone: true,
+  imports: [TitleComponent, FirstBlockComponent, SecondBlockComponent, ThirdBlockComponent],
+  templateUrl: './main-content.component.html',
+  styleUrl: './main-content.component.scss'
+})
+export class MainContentComponent {
+
+}
