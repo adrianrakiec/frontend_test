47cf6aab12301f67563211d97014e8211b9cf2f4
adrianrakiec <rakiecadrian17@gmail.com>
2024-07-09 17:36:02 +0200

    feat: configure HttpClient and create sample json file

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -3,16 +3,41 @@ import { TitleComponent } from '../title/title.component';
 import { FirstBlockComponent } from '../first-block/first-block.component';
 import { SecondBlockComponent } from '../second-block/second-block.component';
 import { ThirdBlockComponent } from '../third-block/third-block.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [TitleComponent, FirstBlockComponent, SecondBlockComponent, ThirdBlockComponent],
+  imports: [
+    TitleComponent,
+    FirstBlockComponent,
+    SecondBlockComponent,
+    ThirdBlockComponent,
+  ],
   templateUrl: './main-content.component.html',
-  styleUrl: './main-content.component.scss'
+  styleUrl: './main-content.component.scss',
 })
 export class MainContentComponent {
-  selectedOption: string = "";
+  quotes: string[] = [];
+  selectedOption: string = '';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.loadQuotes();
+  }
+
+  loadQuotes() {
+    this.http.get<string[]>('quotes.json').subscribe({
+      next: (data) => {
+        this.quotes = data;
+        console.log(this.quotes);
+      },
+      error: (error) => {
+        console.error('Błąd ładowania pliku JSON', error);
+      },
+    });
+  }
 
   handleDataEvent(data: string) {
     this.selectedOption = data;
