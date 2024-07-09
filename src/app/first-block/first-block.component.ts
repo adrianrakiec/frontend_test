b3abcdf1b293dfa3307cf5019cd94bdeede5f737
adrianrakiec <rakiecadrian17@gmail.com>
2024-07-09 15:35:54 +0200

    build: create static first-block component

---
+++ src/app/first-block/first-block.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-block',
+  standalone: true,
+  imports: [],
+  templateUrl: './first-block.component.html',
+  styleUrl: './first-block.component.scss',
+})
+export class FirstBlockComponent {
+  selectedOption: string = '';
+
+  onOptionChange(option: string) {
+    this.selectedOption = option;
+    console.log(this.selectedOption);
+  }
+}
